c2c093d8e10e7464563bca4a3e681aa19aa14301
Mohamed Elzoghby <36212318+mohamedelzoghby@users.noreply.github.com>
2024-09-24 13:55:20 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region to deploy resources"
+  type        = string
+  default     = "us-central1"  # Optional default
+}
