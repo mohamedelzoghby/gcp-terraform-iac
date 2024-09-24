ae58a61d5cc013a13dcab8de1ae9f784b762daa9
Mohamed Elzoghby <36212318+mohamedelzoghby@users.noreply.github.com>
2024-09-24 14:06:07 -0400

    Create outputs.tf

---
+++ modules/network/outputs.tf
@@ -0,0 +1,18 @@
+# outputs.tf - Network Module
+#
+# This file defines the outputs from the network module.
+# It provides the network ID of the created VPC and the list of subnet IDs.
+# These outputs are useful for other parts of the infrastructure that
+# need to reference the created VPC and subnets.
+
+# Output the ID of the VPC network
+output "network_id" {
+  description = "The ID of the VPC network"
+  value       = google_compute_network.vpc_network.id
+}
+
+# Output the list of Subnet IDs
+output "subnet_ids" {
+  description = "The IDs of the created subnets"
+  value       = google_compute_subnetwork.subnetwork[*].id
+}
