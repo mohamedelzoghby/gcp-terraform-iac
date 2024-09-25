cabfccf70ef1474e95cdb86de0bb6239d87a1f3f
Mohamed Elzoghby <36212318+mohamedelzoghby@users.noreply.github.com>
2024-09-25 12:01:47 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,3 +13,48 @@ variable "region" {
   default     = "us-central1"
 }
 
+# Declare the variables for the network module
+variable "network_name" {
+  description = "The name of the VPC network"
+  type        = string
+}
+
+variable "subnets" {
+  description = "A list of subnets to create"
+  type = list(object({
+    name = string
+    cidr = string
+  }))
+}
+
+# Declare the variables for the compute module
+variable "instance_name" {
+  description = "Base name for the VM instance"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Number of VM instances"
+  type        = number
+}
+
+variable "machine_type" {
+  description = "The machine type for VM instances"
+  type        = string
+}
+
+variable "image" {
+  description = "The disk image to use for the VM instance(s)"
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone where the VM instance(s) will be deployed"
+  type        = string
+}
+
+variable "service_account_email" {
+  description = "The service account email to associate with VM instances"
+  type        = string
+}
+
